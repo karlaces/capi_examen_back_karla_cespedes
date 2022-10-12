93bae6160c7affa0254e1d0268e2548fc86edfed
karlaces <karla.ces@outlook.com>
2022-10-12 17:55:44 -0500

    consultar usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'fecha_nacimiento'
     ];
 
     /**
@@ -41,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function userdomicilio(){
+        return $this->hasOne('App\Models\UserDomicilio', 'user_id', 'id');
+    }
 }
